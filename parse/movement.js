ed6f65e6ecca544697646fb6d640d893e007717f
Traines <git@traines.eu>
2026-06-06 19:01:48 +0000

    regression from migration to newest motis API version

---
+++ parse/movement.js
@@ -0,0 +1,52 @@
+// todo: what is m.dirGeo? maybe the speed?
+// todo: what is m.stopL?
+// todo: what is m.proc? wut?
+// todo: what is m.pos?
+// todo: what is m.ani.dirGeo[n]? maybe the speed?
+// todo: what is m.ani.proc[n]? wut?
+const parseMovement = (ctx, m) => { // m = raw movement
+	const {profile, opt} = ctx;
+
+	const res = {
+		direction: null,
+		tripId: m.trips[0].tripId || null,
+		line: m.displayName || null,
+		location: m.pos
+			? {
+				type: 'location',
+				latitude: m.pos.y / 1000000,
+				longitude: m.pos.x / 1000000,
+			}
+			: null,
+		// todo: stopL[0] is the first of the trip! -> filter out
+		nextStopovers:
+            m.stopL
+            	.filter(s => Boolean(s.location))
+            	.map(s => profile.parseStopover(ctx, s, m.date)),
+		frames: [],
+	};
+
+	if (m.ani) {
+		// todo: ani.dirGeo, ani.fLocX, ani.proc, ani.procAbs, ani.state, ani.stcOutputX
+
+		if (Array.isArray(m.ani.mSec)) {
+			for (let i = 0; i < m.ani.mSec.length; i++) {
+				res.frames.push({
+					origin: m.ani.fromLocations[i] || null,
+					destination: m.ani.toLocations[i] || null,
+					t: m.ani.mSec[i],
+				});
+			}
+		}
+
+		if (opt.polylines) {
+			res.polyline = profile.parsePolyline(ctx, m.polyline);
+		}
+	}
+
+	return res;
+};
+
+export {
+	parseMovement,
+};
